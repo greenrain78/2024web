79f0e98428c6a96cccb82d4c69a9686498e42ed9
dwoong <jkmps7206@gmail.com>
2024-05-25 18:05:22 +0900

    전체적인 수정

--- html/setting.js
+++ html/setting.js
@@ -11,6 +11,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // 배경 음악 요소 가져오기
     const backgroundMusic = document.getElementById('background_music');
 
+    // 초기 배경 음악 재생
+    backgroundMusic.play();
+
     // 음악 선택 버튼 클릭 이벤트
     musicButton.addEventListener('click', function() {
         hideAllOptions(); // 모든 옵션 숨기기
@@ -26,7 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // 소리제거 버튼 클릭 이벤트
     muteButton.addEventListener('click', function() {
         if (backgroundMusic.paused) {
-            console.log(backgroundMusic)
             backgroundMusic.play();
         } else {
             backgroundMusic.pause();
@@ -38,18 +40,13 @@ document.addEventListener('DOMContentLoaded', function() {
         musicOptions.classList.add('hidden');
         ballOptions.classList.add('hidden');
     }
-
-    // 배경음악 자동 재생을 위한 로직은 제거됨
-    // 사용자 인터랙션을 통해 배경음악을 제어하세요
 });
 
 // 음악 변경 함수
 function changeMusic(musicFile) {
     const backgroundMusic = document.getElementById('background_music');
     backgroundMusic.src = musicFile;
-    if(backgroundMusic.paused) {
-        backgroundMusic.play();
-    }
+    backgroundMusic.play();
 }
 
 // 공 선택 함수
